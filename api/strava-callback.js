03314e220b26afedab42cea6f71ce21cce09a361
Stephanie Movahhed <smovahhed@gmail.com>
2026-04-26 16:02:59 -0700

    Add Strava OAuth

---
+++ api/strava-callback.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const clientId = process.env.STRAVA_CLIENT_ID;
+  const clientSecret = process.env.STRAVA_CLIENT_SECRET;
+
+  const tokenRes = await fetch('https://www.strava.com/oauth/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code,
+      grant_type: 'authorization_code',
+    }),
+  });
+
+  const tokenData = await tokenRes.json();
+  const accessToken = tokenData.access_token;
+
+  const activitiesRes = await fetch(
+    'https://www.strava.com/api/v3/athlete/activities?per_page=20',
+    { headers: { Authorization: `Bearer ${accessToken}` } }
+  );
+
+  const activities = await activitiesRes.json();
+  const runs = activities.filter(a => a.type === 'Run').length;
+  const lif
